1be66468ee2354c1b10d05b14bed48f199b73b5a
Rasluka <vargklee@hotmail.com>
2024-09-11 22:34:39 -0600

    Adding fonts and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,24 @@ import { IMovie } from "./types/movie";
 export default async function Home() {
   const apiKey = process.env.MOVIE_DB_API_KEY;
   const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${apiKey}`;
-  const data = await fetch(url);
-  const trendingMovies = await data.json();
+
+  let trendingMovies: IMovie[] | null = null;
+
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch, status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    trendingMovies = data.results;
+  } catch (error) {
+    trendingMovies = [];
+  }
 
   return (
     <>
-      <div className="relative p-4 w-3/6 mx-auto">
+      <div className="relative p-4">
         <div className="absolute inset-0 bg-[url('/images/movie.jpg')] bg-cover"></div>
 
         <div className="absolute inset-0 bg-blue-950 opacity-70"></div>
@@ -25,12 +37,29 @@ export default async function Home() {
         </div>
       </div>
 
-      <h2 className="text-4xl text-center w-3/6 mx-auto mt-5">Trending Now</h2>
+      <div className="flex my-6">
+        <h2 className="text-4xl">Trending</h2>
+        <div className="btn-group btn-group-scrollable">
+          <input
+            type="radio"
+            name="options"
+            data-content="Today"
+            className="btn"
+          />
+          <input
+            type="radio"
+            name="options"
+            data-content="This Week"
+            className="btn"
+          />
+        </div>
+      </div>
 
-      <div className="flex overflow-x-auto scrollbar w-3/6 mx-auto p-4">
-        {trendingMovies.results.map((aMovie: IMovie) => (
-          <MovieCard aMovie={aMovie} key={aMovie.id} />
-        ))}
+      <div className="flex overflow-x-auto scrollbar p-4">
+        {trendingMovies &&
+          trendingMovies.map((aMovie: IMovie) => (
+            <MovieCard aMovie={aMovie} key={aMovie.id} />
+          ))}
       </div>
     </>
   );

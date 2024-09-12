c20433e6f903912bca501f28c03ce61d4af535ac
Rasluka <vargklee@hotmail.com>
2024-09-11 20:13:25 -0600

    Adding homepage

--- src/app/components/MovieList.tsx
+++ src/app/components/MovieList.tsx
@@ -2,17 +2,10 @@
 import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 import MovieCard from "./MovieCard";
-
-type Movie = {
-  id: number;
-  original_title: string;
-  backdrop_path: string;
-  release_date: string;
-  overview: string;
-};
+import { IMovie } from "../types/movie";
 
 export default function MovieList() {
-  const [searchResult, setSearchResult] = useState<Movie[]>([]);
+  const [searchResult, setSearchResult] = useState<IMovie[]>([]);
 
   const onSubmit = (value: string) => {
     console.log(value);
@@ -21,7 +14,7 @@ export default function MovieList() {
 
   const apiFetch = async (searchValue: string) => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/search/movie?query=${searchValue}&api_key=a258ba07c5eac02902bf0152801d54b4`
+      `https://api.themoviedb.org/3/search/movie?query=${searchValue}&api_key=`
     );
     const actualData = await data.json();
     console.log(actualData);
@@ -32,7 +25,7 @@ export default function MovieList() {
     <>
       <SearchBar onSubmit={onSubmit} />
 
-      {searchResult.map((aMovie: Movie) => {
+      {searchResult.map((aMovie: IMovie) => {
         return <MovieCard aMovie={aMovie} key={aMovie.id} />;
       })}
     </>
--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import { IMovie } from "../types/movie";
 
-function Page() {
-  return <div>Movies Page</div>;
+function MoviesPage() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch("/api/movies");
+      const data = await response.json();
+      setTrendingMovies(data);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <div className="relative p-4 w-3/6 mx-auto">
+        <div className="absolute inset-0 bg-[url('/images/movie.jpg')] bg-cover"></div>
+
+        <div className="absolute inset-0 bg-blue-950 opacity-70"></div>
+
+        <div className="relative z-10 p-10">
+          <h1 className="text-6xl mt-5">Welcome.</h1>
+          <h1 className="text-4xl mx-auto">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </h1>
+        </div>
+      </div>
+
+      <h2 className="text-4xl text-center w-3/6 mx-auto mt-5">Trending Now</h2>
+
+      <div className="flex overflow-x-auto scrollbar w-3/6 mx-auto p-4">
+        {trendingMovies.map((aMovie: IMovie) => (
+          <MovieCard aMovie={aMovie} key={aMovie.id} />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Page;
+export default MoviesPage;
